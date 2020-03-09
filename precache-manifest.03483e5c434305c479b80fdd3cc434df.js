e17a96dbf9df9a6731bf7965a9dea10bb1f8f349
Orainsink <ywt1250066597@gmail.com>
2020-03-09 13:54:01 +0800

    Updates

---
+++ precache-manifest.03483e5c434305c479b80fdd3cc434df.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78bc43fa807f3f8c9b3a00c552fb5e5",
+    "url": "/cave2_homepage/index.html"
+  },
+  {
+    "revision": "d302c384d0fc6e2e1e5b",
+    "url": "/cave2_homepage/static/css/2.d8f7068e.chunk.css"
+  },
+  {
+    "revision": "af2ec0160a8c775876e7",
+    "url": "/cave2_homepage/static/css/main.68dbb061.chunk.css"
+  },
+  {
+    "revision": "d302c384d0fc6e2e1e5b",
+    "url": "/cave2_homepage/static/js/2.349750a5.chunk.js"
+  },
+  {
+    "revision": "5f89779e4e602b75cbb1caee687c0d9d",
+    "url": "/cave2_homepage/static/js/2.349750a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2ec0160a8c775876e7",
+    "url": "/cave2_homepage/static/js/main.ec082955.chunk.js"
+  },
+  {
+    "revision": "f52a7ff0c483251ab935",
+    "url": "/cave2_homepage/static/js/runtime-main.aabec2e4.js"
+  },
+  {
+    "revision": "1446171e2598f180061fd0d7a674825a",
+    "url": "/cave2_homepage/static/media/bg-1.1446171e.jpg"
+  },
+  {
+    "revision": "fceb9ab91ad80660a823c62cbb2782b6",
+    "url": "/cave2_homepage/static/media/bg-2.fceb9ab9.gif"
+  },
+  {
+    "revision": "dbfd26b1e73c059693e1cbe1d7f196b2",
+    "url": "/cave2_homepage/static/media/bg-3.dbfd26b1.jpg"
+  },
+  {
+    "revision": "dd3d3a1e1a49a5ce2c7888ab31c0d7af",
+    "url": "/cave2_homepage/static/media/bg-4.dd3d3a1e.jpg"
+  },
+  {
+    "revision": "1569e4ea8f691b40314db56c52dc57c7",
+    "url": "/cave2_homepage/static/media/bg-5.1569e4ea.jpg"
+  },
+  {
+    "revision": "ddad1bff1d6b5f5dc3dacbdd5d982822",
+    "url": "/cave2_homepage/static/media/bg-6.ddad1bff.jpg"
+  },
+  {
+    "revision": "117b7980689124c76337b63d23378752",
+    "url": "/cave2_homepage/static/media/bg-7.117b7980.jpg"
+  },
+  {
+    "revision": "fd6c418d7aaf4e469977d7ff1c2793be",
+    "url": "/cave2_homepage/static/media/ceiling-2.fd6c418d.png"
+  },
+  {
+    "revision": "f898d357dbbd1f7a4d2dbb06bdb74750",
+    "url": "/cave2_homepage/static/media/ceiling-3.f898d357.png"
+  },
+  {
+    "revision": "34f2a7bf699b8e171317cc41da68fccc",
+    "url": "/cave2_homepage/static/media/ceiling-4.34f2a7bf.png"
+  },
+  {
+    "revision": "e96398daf11d1bad573a4cc5012e40be",
+    "url": "/cave2_homepage/static/media/ceiling-5.e96398da.png"
+  },
+  {
+    "revision": "76499b266ff3f2e7a76bd0c7c827c306",
+    "url": "/cave2_homepage/static/media/ceiling-6.76499b26.png"
+  },
+  {
+    "revision": "1619d4f090981f5e357c620b2d869def",
+    "url": "/cave2_homepage/static/media/down-bg.1619d4f0.png"
+  },
+  {
+    "revision": "bf9743bf36a936593e722cd946d51f8b",
+    "url": "/cave2_homepage/static/media/floor-1.bf9743bf.png"
+  },
+  {
+    "revision": "591126923443b3715ec566ad396fad6f",
+    "url": "/cave2_homepage/static/media/floor-2.59112692.png"
+  },
+  {
+    "revision": "291eff0168c18bef1940a1212885c236",
+    "url": "/cave2_homepage/static/media/floor-3.291eff01.png"
+  },
+  {
+    "revision": "6809256034fc01962d53ace10afe4cab",
+    "url": "/cave2_homepage/static/media/floor-4.68092560.png"
+  },
+  {
+    "revision": "8e767bf239ffcc07205a4b180ceea5d3",
+    "url": "/cave2_homepage/static/media/floor-5.8e767bf2.png"
+  },
+  {
+    "revision": "93e6643d9a6148c20018d1e3f245f35b",
+    "url": "/cave2_homepage/static/media/floor-6.93e6643d.png"
+  },
+  {
+    "revision": "42f2a88a15c21ac5ef76937cee7a421b",
+    "url": "/cave2_homepage/static/media/gold-coin-2.42f2a88a.png"
+  },
+  {
+    "revision": "fe0dfd6eb64d2ce38f8ac28b165d0c6c",
+    "url": "/cave2_homepage/static/media/gold-coin-3.fe0dfd6e.png"
+  },
+  {
+    "revision": "ee3b45a0d3892ba4ce4b8d618c704095",
+    "url": "/cave2_homepage/static/media/gold-coin.ee3b45a0.gif"
+  },
+  {
+    "revision": "30a52dc43399bbbde7df2c810beffb8c",
+    "url": "/cave2_homepage/static/media/hand.30a52dc4.png"
+  },
+  {
+    "revision": "c0472a04e8b3de33447a21c987a57eb0",
+    "url": "/cave2_homepage/static/media/img.c0472a04.png"
+  },
+  {
+    "revision": "ad9bd149c83056cbd7e6f05a0b5764bb",
+    "url": "/cave2_homepage/static/media/master.ad9bd149.png"
+  },
+  {
+    "revision": "64f0e0db763ba50f2f1ceb5235b0b62a",
+    "url": "/cave2_homepage/static/media/note-2.64f0e0db.gif"
+  },
+  {
+    "revision": "61d4bde47f1f26d05c1ec13d51bfa0a8",
+    "url": "/cave2_homepage/static/media/note-3.61d4bde4.gif"
+  },
+  {
+    "revision": "787dc4a863107a29a2b09dcf35d87535",
+    "url": "/cave2_homepage/static/media/note-4.787dc4a8.gif"
+  },
+  {
+    "revision": "654eb217c8b158dec90ea47d6882268f",
+    "url": "/cave2_homepage/static/media/note-5.654eb217.gif"
+  },
+  {
+    "revision": "74e0ed673a046514affb5f9a86f07e59",
+    "url": "/cave2_homepage/static/media/pop-gzh.74e0ed67.png"
+  },
+  {
+    "revision": "484bd0de2f6b2e3dc8d4b2124749d0cc",
+    "url": "/cave2_homepage/static/media/pop-kf.484bd0de.png"
+  },
+  {
+    "revision": "0ff3f6582523eae41e97793f1513febc",
+    "url": "/cave2_homepage/static/media/rockery.0ff3f658.png"
+  },
+  {
+    "revision": "ed2e2d8f6c1549c1cee686e4eb04a0ed",
+    "url": "/cave2_homepage/static/media/rotate2.ed2e2d8f.png"
+  },
+  {
+    "revision": "8ba8bb69121716e6b3511cc78a511e94",
+    "url": "/cave2_homepage/static/media/s-1.8ba8bb69.jpg"
+  },
+  {
+    "revision": "bdfb69318dd305a524d801b8de7f57ea",
+    "url": "/cave2_homepage/static/media/s-2.bdfb6931.jpg"
+  },
+  {
+    "revision": "dec60648190009dfdc763d1304ce726f",
+    "url": "/cave2_homepage/static/media/selling-point-00.dec60648.png"
+  },
+  {
+    "revision": "f84a6568f941823429ab42f0876ad6f9",
+    "url": "/cave2_homepage/static/media/selling-point-01.f84a6568.png"
+  },
+  {
+    "revision": "74cca3c33eaa82f240e6073b845fb5be",
+    "url": "/cave2_homepage/static/media/selling-point-02.74cca3c3.png"
+  },
+  {
+    "revision": "00a2f543eadbb89d82b52a410297dcad",
+    "url": "/cave2_homepage/static/media/selling-point-03.00a2f543.png"
+  },
+  {
+    "revision": "6f6adb23a7d53a9088a491051fb3e500",
+    "url": "/cave2_homepage/static/media/selling-point-04.6f6adb23.png"
+  },
+  {
+    "revision": "3c49331f9363a0e9887f69de405f97cc",
+    "url": "/cave2_homepage/static/media/slogan.3c49331f.png"
+  },
+  {
+    "revision": "f2e1caf2809842481aa96c1309f0fed9",
+    "url": "/cave2_homepage/static/media/staff.f2e1caf2.png"
+  }
+]);
